4f811617dd471a1c1b56ea6a5966717bedb67b3f
chintanpatel1986 <chintanpatel2013@icloud.com>
2024-12-26 20:13:34 -0500

    Add EmployeeController and basic employee management views
    Introduced `EmployeeController` to handle CRUD operations for employees, including listing, adding, updating, and deleting employee records. Added a placeholder JSP view (`employee-form.jsp`) for employee management functionality. These changes lay the groundwork for the employee management feature.

---
+++ src/main/java/org/chintanpatel/app/controller/EmployeeController.java
@@ -0,0 +1,54 @@
+package org.chintanpatel.app.controller;
+
+import jakarta.validation.Valid;
+import org.chintanpatel.app.model.Employee;
+import org.chintanpatel.app.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/getEmployee")
+    public String getEmployee(Model model) {
+        model.addAttribute("employee", new Employee());
+        model.addAttribute("employeeList", employeeService.getAllEmployeeList());
+        return "employee-form";
+    }
+
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@Valid @ModelAttribute("employee")Employee employee, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("employeeList", employeeService.getAllEmployeeList());
+            return "employee-form";
+        }
+        employeeService.addEmployee(employee);
+        return "redirect:/employees/getEmployee";
+    }
+
+    @GetMapping("/manageEmployee")
+    public String manageEmployee(@RequestParam("employeeId")Long employeeId, Model model) {
+        if (employeeId != null) {
+            model.addAttribute("employee", employeeService.getEmployeeById(employeeId));
+            model.addAttribute("employeeList", employeeService.getAllEmployeeList());
+        }
+        return "employee-form";
+    }
+
+    @GetMapping("/deleteEmployee")
+    public String deleteEmployee(@RequestParam("employeeId")Long employeeId) {
+        if (employeeId != null) {
+            employeeService.deleteEmployeeById(employeeId);
+        }
+        return "redirect:/employees/getEmployee";
+    }
+}
