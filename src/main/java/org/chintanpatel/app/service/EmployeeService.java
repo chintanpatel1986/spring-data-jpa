fa164b4e51f8da21b20b233fad445f15bf694a73
chintanpatel1986 <chintanpatel2013@icloud.com>
2024-12-26 20:06:50 -0500

    Add Employee service interface and implementation
    Introduced `EmployeeService` interface and its implementation `EmployeeServiceImpl` to manage employee operations. Implemented methods for adding, retrieving, and deleting employees, with transactional support. This enhances the service layer of the application for handling employee-related logic.

---
+++ src/main/java/org/chintanpatel/app/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package org.chintanpatel.app.service;
+
+import org.chintanpatel.app.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    void addEmployee(Employee employee);
+
+    List<Employee>getAllEmployeeList();
+
+    Employee getEmployeeById(Long employeeId);
+
+    void deleteEmployeeById(Long employeeId);
+}
