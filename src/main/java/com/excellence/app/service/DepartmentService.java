09d35f44c7110375826b96099d14df11b64f9124
excellence <chintanpatel2013@icloud.com>
2025-03-25 11:12:50 +0530

    Create Department service and their implementation file and add transaction based methods for this application.

---
+++ src/main/java/com/excellence/app/service/DepartmentService.java
@@ -0,0 +1,16 @@
+package com.excellence.app.service;
+
+import com.excellence.app.model.Department;
+
+import java.util.List;
+
+public interface DepartmentService {
+
+    void addDepartment(Department department);
+
+    List<Department>getAllDepartmentList();
+
+    Department getDepartmentById(Long departmentId);
+
+    void deleteDepartmentById(Long departmentId);
+}
